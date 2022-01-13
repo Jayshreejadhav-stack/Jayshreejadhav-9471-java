a48cb32c35aaafd07c5a7be10d0e4a2d240a047e
Jayshreejadhav-stack <95115736+Jayshreejadhav-stack@users.noreply.github.com>
2022-01-14 00:27:58 +0530

    Add files via upload

---
+++ Anudip Practicals/AddColor.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class AddColor{
+public static void main(String args[])
+{
+List<String> list_Strings = new ArrayList<String>();
+list_Strings.add("Black");
+list_Strings.add("Blue");
+list_Strings.add("Green");
+list_Strings.add("Maroon");
+list_Strings.add("Grey");
+System.out.println(list_Strings);
+list_Strings.set(2,"pink");
+}}
