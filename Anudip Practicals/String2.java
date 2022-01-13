d2bbca5fd547649959bedf93700314544d3dccb8
Jayshreejadhav-stack <95115736+Jayshreejadhav-stack@users.noreply.github.com>
2022-01-13 22:12:35 +0530

    Add files via upload

---
+++ Anudip Practicals/String2.java
@@ -0,0 +1,10 @@
+public class String2{
+public static void main(String args[]){
+String str1 = "Welcome";
+char[] arr = {'t','o'};
+String str2 = new String(arr);
+String str3 = new String("today's lecture");
+System.out.println(str1);
+System.out.println(str2);
+System.out.println(str3);
+}}
